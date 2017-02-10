5289122f1ccb09bf18b13a7d114e48fd902f4b66
yuckYew <yusukechayama@gmail.com>
2017-02-10 18:08:28 +0900

    official tutorial

--- Python/Flask/flaskr/flaskr/flaskr.py
+++ Python/Flask/flaskr/flaskr/flaskr.py
@@ -17,6 +17,10 @@ app.config.update(dict(
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 
+"""
+DB / Application Content
+"""
+
 def connect_db():
     """Connects to the specific database."""
     rv = sqlite3.connect(app.config['DATABASE'])
@@ -37,6 +41,7 @@ def initdb_command():
     init_db()
     print("Initialized the database.")
 
+
 def get_db():
     """Opens a new database connection if there is none yet
     for the current application context.
@@ -52,6 +57,9 @@ def close_db(error):
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
 
+"""
+Routing
+"""
 @app.route('/')
 def show_entries():
     db = get_db()
@@ -59,14 +67,35 @@ def show_entries():
     entries = cur.fetchall()
     return render_template('show_entries.html', entries=entries)
 
-@app.route('/add', methos=['POST'])
+
+@app.route('/add', methods=['POST'])
 def add_entry():
     if not session.get('logged_in'):
         abort(401)
     db = get_db()
     db.execute('insert into entries (title, text) values (?, ?)',
-        [request.form['title'], request.form['text']])
+                [request.form['title'], request.form['text']])
     db.commit()
     flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
     return redirect(url_for('show_entries'))
 
