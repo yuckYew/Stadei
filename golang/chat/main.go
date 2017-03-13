2d45c454d0190000a6149426573cea19445eeecb
yuckYew <yusukechayama@gmail.com>
2017-03-13 15:01:04 +0900

    Load from template(End of sec 1.1)

--- golang/chat/main.go
+++ golang/chat/main.go
@@ -3,22 +3,30 @@ package main
 import (
 	"log"
 	"net/http"
+	"text/template"
+	"path/filepath"
+	"sync"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-      <html>
-        <head>
-          <title>chat!</title>
-        </head>
-        <body>
-          Let's Chat!
-        </body>
-      </html>
-    `))
-	})
+// templ is one template
+type templateHandler struct {
+	once			sync.Once
+	filename	string
+	templ			*template.Template
+}
 
+// ServeHTTP processes HTTP requests
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ =
+			template.Must(template.ParseFiles(filepath.Join("templates",
+				t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
+
+func main() {
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 	// Start Web server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
